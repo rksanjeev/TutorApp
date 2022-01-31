120381675a20ff8d920c63c5096678b83a15b256
Sanjeev Kumar <r.sanjeev@outlook.com>
2022-01-31 22:22:26 +0530

    Added index page

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from re import template
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+class IndexPage(TemplateView):
+    template_name = 'home.html'
