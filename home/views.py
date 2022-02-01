03210da479fc04365e427c5e4953328624e7f46f
Sanjeev Kumar <r.sanjeev@outlook.com>
2022-02-01 21:53:41 +0530

    Added supporting pages and routing

--- home/views.py
+++ home/views.py
@@ -5,3 +5,12 @@ from django.views.generic import TemplateView
 
 class IndexPage(TemplateView):
     template_name = 'home.html'
+
+class AboutPage(TemplateView):
+    template_name = 'about.html'    
+
+class ContactPage(TemplateView):
+    template_name = 'contact.html'
+
+class PrivacyPage(TemplateView):
+    template_name = 'privacy.html'
