5433ce525f8e75ebf874b0497f0f6161739fe7bb
Sanjeev Kumar <r.sanjeev@outlook.com>
2022-02-01 23:11:56 +0530

    Refactoring

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,8 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.IndexPage.as_view(), name='index' ),
-    path('about', views.AboutPage.as_view(), name='about'),
-    path('contact', views.ContactPage.as_view(), name='contact'),
-    path('privacy-policy', views.PrivacyPage.as_view(), name='privacy'),
+    path('/about', views.AboutPage.as_view(), name='about'),
+    path('/contact', views.ContactPage.as_view(), name='contact'),
+    path('/privacy-policy', views.PrivacyPage.as_view(), name='privacy'),
 ]
 
